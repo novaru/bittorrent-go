74c5c53468f181449843fedcf5cbd3546ddddfd8
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2024-10-21 14:04:08 +0700

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -5,48 +5,30 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
 var _ = json.Marshal
 
-func decodeInt(bencodedString string) (int, error) {
-	return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-}
+func decodeList(bencodedString string) ([]interface{}, string, error) {
+	decodedList := make([]interface{}, 0)
+	bencodedString = bencodedString[1:] // Remove the leading 'l'
 
-func decodeStr(bencodedString string) (string, error) {
-	var firstColonIndex int
-
-	for i := 0; i < len(bencodedString); i++ {
-		if bencodedString[i] == ':' {
-			firstColonIndex = i
-			break
-		}
-	}
-
-	lengthStr := bencodedString[:firstColonIndex]
-
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		return "", err
-	}
-
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-}
-
-func decodeList(bencodedString string) ([]interface{}, error) {
-	var decodedList []interface{}
-	bencodedString = bencodedString[1 : len(bencodedString)-1]
-
-	for len(bencodedString) > 0 {
+	for len(bencodedString) > 0 && bencodedString[0] != 'e' {
 		decoded, rest, err := decodeBencode(bencodedString)
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 		decodedList = append(decodedList, decoded)
 		bencodedString = rest
 	}
-	return decodedList, nil
+
+	if len(bencodedString) == 0 {
+		return nil, "", fmt.Errorf("unterminated list")
+	}
+
+	return decodedList, bencodedString[1:], nil // Remove the trailing 'e'
 }
 
 func decodeBencode(bencodedString string) (interface{}, string, error) {
@@ -54,30 +36,53 @@ func decodeBencode(bencodedString string) (interface{}, string, error) {
 		return nil, "", fmt.Errorf("empty string")
 	}
 
-	first := bencodedString[0]
-
-	switch {
-	case first == 'l':
-		list, err := decodeList(bencodedString)
-		if err != nil {
-			return nil, "", err
-		}
-		return list, bencodedString[len(bencodedString):], nil
-	case first >= '0' && first <= '9':
-		str, err := decodeStr(bencodedString)
-		if err != nil {
-			return nil, "", err
-		}
-		return str, bencodedString[len(str)+len(strconv.Itoa(len(str)))+1:], nil
-	case first == 'i':
-		num, err := decodeInt(bencodedString)
-		if err != nil {
-			return nil, "", err
-		}
-		return num, bencodedString[len(strconv.Itoa(num))+2:], nil
+	switch bencodedString[0] {
+	case 'i':
+		num, rest, err := decodeInt(bencodedString)
+		return num, rest, err
+	case 'l':
+		list, rest, err := decodeList(bencodedString)
+		return list, rest, err
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		str, rest, err := decodeStr(bencodedString)
+		return str, rest, err
+	case 'e':
+		return nil, bencodedString[1:], nil // Handle empty list or end of list
 	default:
-		return nil, "", fmt.Errorf("unknown type: %s", string(first))
+		return nil, "", fmt.Errorf("unknown type: %c", bencodedString[0])
 	}
+}
+
+func decodeInt(bencodedString string) (int64, string, error) {
+	endIndex := strings.IndexByte(bencodedString, 'e')
+	if endIndex == -1 {
+		return 0, "", fmt.Errorf("invalid integer encoding")
+	}
+	num, err := strconv.ParseInt(bencodedString[1:endIndex], 10, 64)
+	if err != nil {
+		return 0, "", err
+	}
+	return num, bencodedString[endIndex+1:], nil
+}
+
+func decodeStr(bencodedString string) (string, string, error) {
+	colonIndex := strings.IndexByte(bencodedString, ':')
+	if colonIndex == -1 {
+		return "", "", fmt.Errorf("invalid string encoding")
+	}
+
+	length, err := strconv.Atoi(bencodedString[:colonIndex])
+	if err != nil {
+		return "", "", err
+	}
+
+	if colonIndex+1+length > len(bencodedString) {
+		return "", "", fmt.Errorf("string length mismatch")
+	}
+
+	str := bencodedString[colonIndex+1 : colonIndex+1+length]
+	rest := bencodedString[colonIndex+1+length:]
+	return str, rest, nil
 }
 
 func main() {
@@ -92,7 +97,6 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	} else {
